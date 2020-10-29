fe837497f0780f32a7b011c01281136a4ff30e07
NavidMadani <navidmadani4598@gmail.com>
2020-10-29 06:11:35 -0400

    capture toggle

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,12 @@ export default function Home({ pokemon }) {
                   {pokeman.name}
                   </a>
               </Link>
-
+              <div className="relative mx-2 inline-block w-10 mr-2 align-middle select-none transition-all duration-200 ease-in">
+                <input type="checkbox" name="toggle" id={index}  className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"/>
+                <label for="toggle" name="toggle" className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
+              </div>
+              <label for="toggle" class="text-xs text-gray-700">Captured</label>
+              <div class="flex items-center justify-center w-full"></div>
             </li>
           ))}
         </ul>
