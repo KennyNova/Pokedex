5f0b9d771b6ea34b11a6bc07975d1c5113c935ab
Navid Madani <navidmadani4598@gmail.com>
2021-05-15 16:03:11 -0400

    fixed build script. changed color theme

--- component/Layout.js
+++ component/Layout.js
@@ -3,7 +3,11 @@ import Head from 'next/Head'
 
 export default function Layout({ title, children }) {
     return (
+<<<<<<< HEAD
         <div className="bg-gray-400">
+=======
+        <div className='background'>
+>>>>>>> e51c6e5 (fixed build script. changed color theme)
             <Head>
                 <title>{title}</title>
                 <link rel="icon" href="/favicon.ico" />
@@ -11,6 +15,19 @@ export default function Layout({ title, children }) {
             <main className=" pt-8 min-h-screen">
                 {children}
             </main>
+<<<<<<< HEAD
         </div>
     )
 }
+=======
+            <style jsx>{`
+        .background {
+            background-color: #343e46;
+            padding:0px 50px 50px 50px;
+        }
+      `}</style>
+        </div>
+    )
+}
+
+>>>>>>> e51c6e5 (fixed build script. changed color theme)
--- pages/index.js
+++ pages/index.js
@@ -20,23 +20,29 @@ export default function Home({ pokemon }) {
           background-color: #68D391;
           transition: all 0.3s ease-in-out;
         }
+        .secondary-gray{
+          background-color:#282e32;
+        }
+        .black{
+          background-color:#131313;
+        }
       `}</style>
-        <h1 className="text-4xl mb-8 text-center ">NextJS Pokedex</h1>
-        <ul className=" flex -mx-2 flex-wrap ">
+        <h1 className="text-4xl mb-8 text-center text-white">NextJS Pokedex</h1>
+        <ul className=" flex -mx-2 flex-wrap">
           {pokemon.map((pokeman, index) => (
             <li className="w-1/4 h-full flex-wrap" key={index}>
               <Link href={`/pokemon?id=${index + 1}`}>
-                <a className="border p-4 border-gray my-1 mx-2 capitalize flex text-lg bg-gray-300 rounded-md ">
+                <a className=" p-4 border-gray my-1 mx-2 capitalize flex text-lg rounded-md secondary-gray text-white">
                   <img className="w-20 h-20 mr-3" src={pokeman.image} alt={pokeman.name}/>
-                  <span className="mr-2 font-bold">{index + 1}.</span>
+                  <span className="mr-2 font-bold text-white">{index + 1}.</span>
                   {pokeman.name}
                   </a>
               </Link>
               <div className="relative mx-2 inline-block w-10 mr-2 align-middle select-none transition-all duration-200 ease-in">
-                <input type="checkbox" name="toggle" id={index}  className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"/>
-                <label htmlFor="toggle" name="toggle" className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
+                <input type="checkbox" name="toggle" id={index}  className="toggle-checkbox absolute block w-6 h-6 rounded-full black appearance-none cursor-pointer"/>
+                <label htmlFor="toggle" name="toggle" className="toggle-label block overflow-hidden h-6 rounded-full secondary-gray cursor-pointer"></label>
               </div>
-              <label htmlFor="toggle" className="text-xs text-gray-700">Captured</label>
+              <label htmlFor="toggle" className="text-xs text-gray-300">Captured</label>
               <div className="flex items-center justify-center w-full"></div>
             </li>
           ))}
