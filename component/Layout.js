b9637156379d2c42aae6ff6dc288f1fb9978da5e
Navid Madani <navidmadani4598@gmail.com>
2021-05-16 00:50:23 -0400

    finished rebase

--- component/Layout.js
+++ component/Layout.js
@@ -3,11 +3,7 @@ import Head from 'next/Head'
 
 export default function Layout({ title, children }) {
     return (
-<<<<<<< HEAD
-        <div className="bg-gray-400">
-=======
         <div className='background'>
->>>>>>> e51c6e5 (fixed build script. changed color theme)
             <Head>
                 <title>{title}</title>
                 <link rel="icon" href="/favicon.ico" />
@@ -15,19 +11,14 @@ export default function Layout({ title, children }) {
             <main className=" pt-8 min-h-screen">
                 {children}
             </main>
-<<<<<<< HEAD
-        </div>
-    )
-}
-=======
+
             <style jsx>{`
-        .background {
-            background-color: #343e46;
-            padding:0px 50px 50px 50px;
-        }
-      `}</style>
+                .background {
+                    background-color: #343e46;
+                    padding:0px 50px 50px 50px;
+                }
+            `}</style>
         </div>
     )
 }
 
->>>>>>> e51c6e5 (fixed build script. changed color theme)
